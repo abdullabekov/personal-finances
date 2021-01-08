26f79645bd1adaf4fbd1ef6be10fa5b9860932a0
Denis Abdullabekov <mrden@inbox.ru>
2021-01-08 23:45:39 +1000

    Initial commit

---
+++ app/src/main/java/com/abdullabekov/personalfinances/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abdullabekov.personalfinances.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abdullabekov.personalfinances.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
